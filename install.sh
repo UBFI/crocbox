0457953e124d0d560fa24800be35523a912ef0d1
UBFI <69093516+UBFI@users.noreply.github.com>
2020-10-18 06:25:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
-mkdir -p /opt/cumbox
-chmod 777 /opt/cumbox
-wget https://assets.checkra.in/downloads/linux/cli/arm/d751f4b245bd4071c571654607ca4058e9e7dc4a5fa30639024b6067eebf5c3b/checkra1n -O /opt/cumbox/checkra1n
-chmod +x /opt/cumbox/checkra1n
-cp 01-cumbox.rules /etc/udev/rules.d
+echo UBFI 2020 installing crocbox
+apt update
+apt install -y curl
+apt install -y usbmuxd
+apt install -y libimobiledevice6
+apt install -y whiptail
+mkdir -p /opt/crocbox
+chmod 777 /opt/crocbox
+wget https://assets.checkra.in/downloads/linux/cli/arm/d751f4b245bd4071c571654607ca4058e9e7dc4a5fa30639024b6067eebf5c3b/checkra1n -O /opt/crocbox/checkra1n
+chmod +x /opt/crocbox/checkra1n
+echo ACTION=="add", ATTRS{idVendor}=="05ac", ATTRS{idProduct}=="1227", RUN+="/opt/crocbox/checkra1n -c" >> /etc/udev/rules.d/01-crocbox.rules
